a55c59f795f982f77f841e83996924ac29b89bf4
Ilya Nichiporkov <ni4iporkoff@gmail.com>
2024-07-04 17:50:26 +0300

    feat: save search term in localStorage

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,14 +1,41 @@
-import { Component } from 'react';
+import { ChangeEvent, Component, FormEvent } from 'react';
 import './styles.css';
 
-export default class Search extends Component {
+interface ISearchState {
+  inputValue: string;
+}
+
+export default class Search extends Component<
+  Record<string, never>,
+  ISearchState
+> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    };
+  }
+
+  handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    localStorage.setItem('searchInputValue', this.state.inputValue);
+  };
+
   render() {
+    const { inputValue } = this.state;
+
     return (
       <header>
-        <form className="search">
+        <form onSubmit={this.handleFormSubmit} className="search">
           <input
             type="text"
             placeholder="Search pokemon..."
+            value={inputValue}
+            onChange={this.handleInput}
             className="search-input"
           />
           <button type="submit" className="search-btn">
