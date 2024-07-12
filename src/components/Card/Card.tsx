cfba1993495215f24e79054ba60a3fa25c5c2631
Ilya Nichiporkov <ni4iporkoff@gmail.com>
2024-07-12 11:26:46 +0300

    refactor: change Card and Cards components to functional components

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,20 @@
-import { Component } from 'react';
-
 import './styles.css';
 import { ICharacter } from '../../lib/definitions';
 
-export default class Card extends Component<{ charater: ICharacter }> {
-  render() {
-    const { name, height, mass } = this.props.charater;
-    return (
-      <div className="card">
-        <div className="card-item">Name: {name}</div>
-        <div className="card-item">Height: {height}</div>
-        <div className="card-item">Weight: {mass}</div>
-      </div>
-    );
-  }
+interface ICardProps {
+  character: ICharacter;
 }
+
+const Card = ({ character }: ICardProps) => {
+  const { name, height, mass } = character;
+
+  return (
+    <div className="card">
+      <div className="card-item">Name: {name}</div>
+      <div className="card-item">Height: {height}</div>
+      <div className="card-item">Weight: {mass}</div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,22 +1,25 @@
-import { Component } from 'react';
 import Card from '../Card/Card';
 import './styles.css';
 import { ICharacter } from '../../lib/definitions';
 
-export default class Cards extends Component<{ fetchedData: ICharacter[] }> {
-  render() {
-    const charaters = this.props.fetchedData;
-
-    return (
-      <ul className="cards">
-        {charaters.map((charater) => {
-          return (
-            <li key={charater.name}>
-              <Card charater={charater} />
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
+interface ICardsProps {
+  fetchedData: ICharacter[];
 }
+
+const Cards = ({ fetchedData }: ICardsProps) => {
+  const characters = fetchedData;
+
+  return (
+    <ul className="cards">
+      {characters.map((character: ICharacter) => {
+        return (
+          <li key={character.name}>
+            <Card character={character} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Cards;
