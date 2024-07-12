d3ca455f2f9fdbab5adea59d3aedf2ac1722cd03
Ilya Nichiporkov <ni4iporkoff@gmail.com>
2024-07-12 11:51:48 +0300

    feat: change Search component to functional

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,50 +1,41 @@
-import { ChangeEvent, Component, FormEvent } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import './styles.css';
 
 interface ISearchProps {
   handleSearch: (searchedValue: string) => void;
 }
 
-interface ISearchState {
-  inputValue: string;
-}
+const Search = ({ handleSearch }: ISearchProps) => {
+  const [inputValue, setInputValue] = useState<string>(
+    localStorage.getItem('searchValue') || ''
+  );
 
-export default class Search extends Component<ISearchProps, ISearchState> {
-  constructor(props: ISearchProps) {
-    super(props);
-    this.state = {
-      inputValue: localStorage.getItem('searchValue') || '',
-    };
-  }
-
-  handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    this.setState({ inputValue: e.target.value });
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
   };
 
-  handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    this.props.handleSearch(this.state.inputValue);
-    localStorage.setItem('searchValue', this.state.inputValue);
+    handleSearch(inputValue);
+    localStorage.setItem('searchValue', inputValue);
   };
 
-  render() {
-    const { inputValue } = this.state;
+  return (
+    <header>
+      <form onSubmit={handleFormSubmit} className="search">
+        <input
+          type="text"
+          placeholder="Search pokemon..."
+          value={inputValue}
+          onChange={handleInput}
+          className="search-input"
+        />
+        <button type="submit" className="search-btn">
+          Search
+        </button>
+      </form>
+    </header>
+  );
+};
 
-    return (
-      <header>
-        <form onSubmit={this.handleFormSubmit} className="search">
-          <input
-            type="text"
-            placeholder="Search pokemon..."
-            value={inputValue}
-            onChange={this.handleInput}
-            className="search-input"
-          />
-          <button type="submit" className="search-btn">
-            Search
-          </button>
-        </form>
-      </header>
-    );
-  }
-}
+export default Search;
